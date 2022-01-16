5b07346f6beb59c432a0de2d78c70b7300bbf435
Franco Kurachi <francokurachi@gmail.com>
2022-01-15 23:41:02 -0300

    Agregamos las clases correspondientes para mejorar los estilos de los productos

--- js/componentes/productos.js
+++ js/componentes/productos.js
@@ -9,97 +9,91 @@ Vue.component("productos", {
     template: 
 `
 
-<section>
-<h1>Velas</h1>
-    <article class="cards">
-                <div class="cards-detalle">
-                    <h2>Hologram</h2>
-                    <p>$<span>1950</span></p>
-                    <button class="butt agregar">Agregar</button>
-                    <a href="productoAmpliado.html" class="btn butt ampliar ampliar2" >Ampliar</a>
-                </div>    
-                <picture class="cards-img">
-                    <source media="(max-width:768px)" srcset="img/detalle_Hologram.jpg">
-                    <img src="img/producto/Hologram.jpg" alt="">
-                </picture>
-    </article>
-    <article class="cards">
-        <div class="cards-detalle">
-            <h2>Aura</h2>
-            <p>$<span>3200</span></p>
-            <button class="butt agregar">Agregar</button>
-            <a href="productoAmpliado.html" class="btn butt ampliar ampliar2" >Ampliar</a>
-        </div>
-        
-        <picture class="cards-img">
-            <source media="(max-width:768px)" srcset="img/detalle_Aura.jpg">
-            <img src="img/producto/Aura.jpg" alt="">
-        </picture>
-    </article>
-    <article class="cards">
-        <div class="cards-detalle">
-            <h2>Blanca Botanica</h2>
-            <p>$<span>2300</span></p>
-            <button class="butt agregar">Agregar</button>
-            <a href="productoAmpliado.html" class="btn butt ampliar ampliar2" >Ampliar</a>
-        </div>
-
-        <picture class="cards-img">
-            <source media="(max-width:768px)" srcset="img/detalle_BlancaBotanica.jpg">
-            <img src="img/producto/BlancaBotanica.jpg" alt="">
-        </picture>
-    </article>
-    <article class="cards">
-        <div class="cards-detalle">
-            <h2>Byron</h2>
-            <p>$<span>1600</span></p>
-            <button class="butt agregar">Agregar</button>
-            <a href="productoAmpliado.html" class="btn butt ampliar ampliar2" >Ampliar</a>
-        </div>
-
-        <picture class="cards-img">
-            <source media="(max-width:768px)" srcset="img/detalle_Byron.jpg">
-            <img src="img/producto/Byron.jpg" alt="">
-        </picture>
-    </article>
-    <article class="cards">
-        <div class="cards-detalle">
-            <h2>Night Smoke</h2>
-            <p>$<span>1800</span></p>
-            <button class="butt agregar">Agregar</button>
-            <a href="productoAmpliado.html" class="btn butt ampliar ampliar2" >Ampliar</a>
-        </div>
-
-        <picture class="cards-img">
-            <source media="(max-width:768px)" srcset="img/detalle_NightSmoke.jpg">
-            <img src="img/producto/NightSmoke.jpg" alt="">
-        </picture>
-    </article>
-    <article class="cards">
-        <div class="cards-detalle">
-            <h2>Stars Wills</h2>
-            <p>$<span>1800</span></p>
-            <button class="butt agregar">Agregar</button>
-            <a href="productoAmpliado.html" class="btn butt ampliar ampliar2" >Ampliar</a>
-        </div>
-
-        <picture class="cards-img">
-            <source media="(max-width:768px)" srcset="img/detalle_StarsWills.jpg">
-            <img src="img/producto/StarsWills.jpg" alt="">
-        </picture>
-    </article>
-    <article  v-for="item in local" class="cards">
-                <div class="cards-detalle">
-                    <h2>{{item.nombre}}</h2>
-                    <p>$ {{item.precio}}</p>
-                    <button class="butt agregar">Agregar</button>
-                    <a href="" class="btn butt ampliar ampliar2" >Ampliar</a>
-                </div>
-                <picture>
-                        <img :src="item.imagen" :alt="item.descrImg"/>
-                </picture>
+<div>
+    <h1 class="pt-4">Velas</h1>
+    <section id="cards" class="pt-4">
+        <article class="cards shadow-lg">
+            <div class="cards-detalle">
+                <h2>Hologram</h2>
+                <p>$<span>1950</span></p>
+                <button class="btn btn-primary btn-lg">Agregar</button>
+                <button class="btn btn-outline-dark btn-lg">Ampliar</button>
+            </div>    
+            <div class="cards-img">
+                <img src="img/producto/Hologram.jpg" alt="">
+            </div>
         </article>
-</section>
+        <article class="cards shadow-lg">
+            <div class="cards-detalle">
+                <h2>Aura</h2>
+                <p>$<span>3200</span></p>
+                <button class="btn btn-primary btn-lg">Agregar</button>
+                <button class="btn btn-outline-dark btn-lg">Ampliar</button>
+            </div>
+            <div class="cards-img">
+                <source media="(max-width:768px)" srcset="img/detalle_Aura.jpg">
+                <img src="img/producto/Aura.jpg" alt="">
+            </div>
+        </article>
+        <article class="cards shadow-lg">
+            <div class="cards-detalle">
+                <h2>Blanca Botanica</h2>
+                <p>$<span>2300</span></p>
+                <button class="btn btn-primary btn-lg">Agregar</button>
+                <button class="btn btn-outline-dark btn-lg">Ampliar</button>
+            </div>
+            <div class="cards-img">
+                <source media="(max-width:768px)" srcset="img/detalle_BlancaBotanica.jpg">
+                <img src="img/producto/BlancaBotanica.jpg" alt="">
+            </div>
+        </article>
+        <article class="cards shadow-lg">
+            <div class="cards-detalle">
+                <h2>Byron</h2>
+                <p>$<span>1600</span></p>
+                <button class="btn btn-primary btn-lg">Agregar</button>
+                <button class="btn btn-outline-dark btn-lg">Ampliar</button>
+            </div>
+            <div class="cards-img">
+                <source media="(max-width:768px)" srcset="img/detalle_Byron.jpg">
+                <img src="img/producto/Byron.jpg" alt="">
+            </div>
+        </article>
+        <article class="cards shadow-lg">
+            <div class="cards-detalle">
+                <h2>Night Smoke</h2>
+                <p>$<span>1800</span></p>
+                <button class="btn btn-primary btn-lg">Agregar</button>
+                <button class="btn btn-outline-dark btn-lg">Ampliar</button>
+            </div>
+            <div class="cards-img">
+                <img src="img/producto/NightSmoke.jpg" alt="">
+            </div>
+        </article>
+        <article class="cards shadow-lg">
+            <div class="cards-detalle">
+                <h2>Stars Wills</h2>
+                <p>$<span>1800</span></p>
+                <button class="btn btn-primary btn-lg">Agregar</button>
+                <button class="btn btn-outline-dark btn-lg">Ampliar</button>
+            </div>
+            <div class="cards-img">
+                <img src="img/producto/StarsWills.jpg" alt="">
+            </div>
+        </article>
+        <article  v-for="item in local" class="cards shadow-lg">
+            <div class="cards-detalle">
+                <h2>{{item.nombre}}</h2>
+                <p>$ {{item.precio}}</p>
+                <button class="btn btn-primary btn-lg">Agregar</button>
+                <button class="btn btn-outline-dark btn-lg">Ampliar</button>
+            </div>
+            <div>
+                    <img :src="item.imagen" :alt="item.descrImg"/>
+            </div>
+        </article>
+    </section>
+</div>
            
     `,
      
